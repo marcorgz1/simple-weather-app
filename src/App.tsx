7e32369add11fc4a40b3450280bdccaa613240c2
marcorgz1 <marco.valdemoro@gmail.com>
2026-06-26 16:20:02 +0200

    feat: designing the UI to looks modern and user-friendly

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { Search } from 'lucide-react';
 
 const API_KEY = import.meta.env.VITE_API_KEY
 
@@ -22,10 +23,19 @@ function App() {
 
   return (
     <main className="flex flex-col justify-center items-center gap-16">
-      <h1 className="text-4xl">Conoce qué clima hace 🌤️</h1>
-      <form>
-        <input placeholder='Introduce una ciudad...' />
-        <button className="text-white bg-purple-700 py-2 px-4 rounded">Enviar</button>
+      <div className="flex flex-col justify-center items-center gap-8 mb-6">
+        <h1 className="text-4xl">Consulta el tiempo actual de tu ciudad 🌤️</h1>
+        <h3>Busca cualquier ciudad y consulta las condiciones climatológicas al instante.</h3>
+      </div>
+      <form className="flex justify-center items-center gap-20">
+        <input 
+          placeholder='Introduce una ciudad...'
+          className="h-9 min-w-0 rounded-md border border-gray-100/40 bg-transparent px-3 py-1 text-base shadow-xs w-md outline-none selection:bg-primary selection:text-primary-foreground file:inline-flex file:h-7 file:border-0 
+            focus-visible:ring-[3px] focus-visible:ring-gray-400/40" />
+        <button className="flex items-center gap-4 text-white bg-slate-800 py-2 px-4 rounded">
+          <Search />
+          Buscar
+        </button>
         <section>{data}</section>
       </form>
     </main>
